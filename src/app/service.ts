1c65df2a88254977bb3886a49e6be3e2d160ef5b
Katsiaryna <larichevak@gmail.com>
2017-01-09 00:38:44 +0300

    style(service.ts):set token variable

--- src/app/service.ts
+++ src/app/service.ts
@@ -9,8 +9,10 @@ export class HttpService{
     getData(){
     	let headers = new Headers();
         let options = new RequestOptions({ headers: headers });
+        const token = 'Bearer AAAAAAAAAAAAAAAAAAAAALuzygAAAAAAJjhIRVCNYPm%2FWF6%2BsLYaaVqPhaE%3DQZ9dPVXv3NkaNJwPPH1XDxuLv3ORmd8J3BRqCD1I2uEjYqPxr5';
 
-    	headers.append('Authorization', 'Bearer AAAAAAAAAAAAAAAAAAAAALuzygAAAAAAJjhIRVCNYPm%2FWF6%2BsLYaaVqPhaE%3DQZ9dPVXv3NkaNJwPPH1XDxuLv3ORmd8J3BRqCD1I2uEjYqPxr5');
+    	headers.append('Authorization', token);
+    	headers.append('Access-Control-Allow-Origin','*');
 
 	    var url = "http://api.twitter.com/1.1/statuses/user_timeline.json?count=100&screen_name=twitterapi";
 
